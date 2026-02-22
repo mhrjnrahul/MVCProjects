fc8772a4efb2ea022fb6ba95a7ac157177b5881d
mhrjnrahul <maharjanrahul58@gmail.com>
2026-02-22 10:45:56 +0545

    Removed unused file

--- MediStock/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MediStock.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
